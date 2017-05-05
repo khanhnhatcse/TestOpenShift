95a79e57b1ffb4fc86fae736257b7173b41b8dbf
Cao Khánh Nhật <khanhnhat.cse@gmail.com>
2017-05-05 17:21:56 +0700

    test

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo "Xin chao the gioi";
+
+<?php 
+
+try{
+
+    $db = new PDO('mysql:host=172.30.250.109;dbnam=test', 'swift', '51302751');
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Success !";
+} catch(PDOException $e){
+    echo "Fail";
+}
+
